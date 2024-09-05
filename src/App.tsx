73bf2a5aba7275d0034359ae3fe1c34db4eb8a37
juratbekoff <juratbekxudayberganov9@gmail.com>
2024-09-05 08:55:49 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import {Route, Routes} from "react-router-dom";
-import {Home, Stream} from "./pages";
+import {useRef} from 'react';
 
 const App = () => {
+    const videoRef = useRef<any>(null);
+
+    const handleFullscreen = () => {
+        const video = videoRef.current;
+
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen(); // Safari
+        } else if (video.msRequestFullscreen) {
+            video.msRequestFullscreen(); // IE/Edge
+        }
+    };
+
     return (
-        <div className={"text-white"}>
-            <Routes>
-                <Route index element={<Home/>}/>
-                <Route path={"/stream/:streamId"} element={<Stream/>}/>
-            </Routes>
+        <div>
+            <video
+                ref={videoRef}
+                onClick={handleFullscreen}
+                style={{width: '100%', height: '100vh', objectFit: 'cover'}}
+                controls
+            >
+                <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
         </div>
     );
 };
