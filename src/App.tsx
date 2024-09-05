5f44dcef93b2239d3f7203f81dce4a170a6b026e
juratbekoff <juratbekxudayberganov9@gmail.com>
2024-09-05 11:14:27 +0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import {Route, Routes} from "react-router-dom";
 import {Home, Stream} from "./pages";
+import WebApp from "@twa-dev/sdk";
 
 const App = () => {
+
+    const platform = WebApp.platform
+    console.log(platform)
+
     return (
         <div className={"text-white"}>
             <Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import WebApp from "@twa-dev/sdk";
+
+WebApp.ready()
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
