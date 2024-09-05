0946964b28cd0f88e97569b2346e79a526df97f1
juratbekoff <juratbekxudayberganov9@gmail.com>
2024-09-05 11:23:03 +0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import WebApp from "@twa-dev/sdk";
 const App = () => {
 
     const platform = WebApp.platform
-    console.log(platform)
+
+    if (platform !== "android" && platform !== "ios" && platform !== "android_x") {
+        return (
+            <div className={"text-white"}>
+                <h1>You have to use from website!</h1>
+                <button className={"bg-blue-600 rounded p-2"} onClick={() => WebApp.openLink("https://kun.uz")}>
+                    open
+                </button>
+            </div>
+        )
+    }
 
     return (
         <div className={"text-white"}>
