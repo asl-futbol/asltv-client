ed334de80d506e39ee074db3354669dedcfa4671
juratbekoff <juratbekxudayberganov9@gmail.com>
2024-09-05 10:09:37 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import {useRef} from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Home, Stream} from "./pages";
 
 const App = () => {
-    const videoRef = useRef<any>(null);
-
-    const handleFullscreen = () => {
-        const video = videoRef.current;
-
-        if (video.requestFullscreen) {
-            video.requestFullscreen();
-        } else if (video.webkitRequestFullscreen) {
-            video.webkitRequestFullscreen(); // Safari
-        } else if (video.msRequestFullscreen) {
-            video.msRequestFullscreen(); // IE/Edge
-        }
-    };
-
     return (
-        <div>
-            <video
-                ref={videoRef}
-                onClick={handleFullscreen}
-                style={{width: '100%', height: '100vh', objectFit: 'cover'}}
-                controls
-            >
-                <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" type="video/mp4"/>
-                Your browser does not support the video tag.
-            </video>
+        <div className={"text-white"}>
+            <Routes>
+                <Route index element={<Home/>}/>
+                <Route path={"/stream/:streamId"} element={<Stream/>}/>
+            </Routes>
         </div>
     );
 };
