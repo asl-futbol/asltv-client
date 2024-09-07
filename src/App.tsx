e668c8fd480e4f8ae72e49e71738129069ced805
juratbekoff <juratbekxudayberganov9@gmail.com>
2024-09-07 11:14:37 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,32 @@ import {Route, Routes} from "react-router-dom";
 import {Home, Match, Redirect} from "./pages";
 import {useAuthUser, useGetUser, UserType} from "./hooks/user.ts";
 import WebApp from "@twa-dev/sdk";
+import {useEffect} from "react";
 
 const App = () => {
-    const user = WebApp.initDataUnsafe.user
-    const getUserQuery = useGetUser(+user?.id!, user ? true : false)
-    const authUserMutation = useAuthUser()
+    const user = WebApp.initDataUnsafe.user;
+    const authUserMutation = useAuthUser();
+    const getUserQuery = useGetUser(+user?.id!, user ? true : false);
 
-    if (user) {
-        const userData: UserType = getUserQuery.data?.data?.info
-        
-        if (!userData) {
-            authUserMutation.mutate({
-                id: user?.id!,
-                name: user?.first_name,
-                username: user?.username,
-                surname: user?.last_name,
-                registeredBy: "WEB_APP"
-            })
+    useEffect(() => {
+        if (user) {
+
+            const userData: UserType = getUserQuery.data?.data?.info;
+
+            if (!userData && !getUserQuery.isFetching) {
+                authUserMutation.mutate({
+                    id: user?.id!,
+                    name: user?.first_name,
+                    username: user?.username,
+                    surname: user?.last_name,
+                    registeredBy: "WEB_APP",
+                });
+            }
+
+            localStorage.setItem("userId", String(user?.id!));
+            localStorage.setItem("name", String(user?.first_name!));
         }
-
-        localStorage.setItem("userId", String(user?.id!))
-        localStorage.setItem("name", String(user?.first_name!))
-    }
+    }, [user, authUserMutation]); // Only run effect when user changes or mutation
 
     return (
         <div className={"text-white"}>
