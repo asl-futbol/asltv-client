e4bf330cb353c95eeb2e3cd5e24d34ac8adafbda
juratbek <juratbekxudayberganov9@gmail.com>
2024-09-21 17:04:38 +0500

    changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,11 @@
+import {IoIosFootball} from "react-icons/io";
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className={"bg-[#2C2E36] px-4 py-4 text-white w-full shadow-xl"}>
             <Link to={"/"} className={"flex gap-1 items-center"}>
-                {/*<IoIosFootball className={"text-2xl"}/>*/}
+                <IoIosFootball className={"text-2xl"}/>
                 <h1 className={"text-xl "}>ASL TV</h1>
             </Link>
         </div>
--- src/pages/Match.tsx
+++ src/pages/Match.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {MdOutlineArrowBackIos} from "react-icons/md";
-import {useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import "plyr-react/plyr.css";
 import {useGetSingleMatch} from "../hooks/match.ts";
 import {MatchType} from "../types/match";
@@ -10,7 +9,7 @@ import Viewers from "../components/viewers.tsx";
 import {VideoPlayer} from "../components";
 
 const Match: React.FC = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const {matchId} = useParams();
 
@@ -29,12 +28,12 @@ const Match: React.FC = () => {
         return <h1 className={"text-center text-white/80"}>Nimadir xato ketdi, keyinroq qayta urining!</h1>
     }
 
-   
+
     return (
         <div className="flex flex-col gap-1 text-white">
             <div className={"max-lg:fixed w-full flex flex-col bg-[#272727]"}>
                 <div className={"flex gap-3  items-center h-full p-3 bg-[#2C2E36]"}>
-                    <MdOutlineArrowBackIos className={"text-xl cursor-pointer"} onClick={() => navigate('/')}/>
+                    {/*<MdOutlineArrowBackIos className={"text-xl cursor-pointer"} onClick={() => navigate('/')}/>*/}
                     <div className={"flex flex-col gap-1"}>
                         <span
                             className={"text-base font-bold text-white/90"}
